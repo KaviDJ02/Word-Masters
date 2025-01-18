36076882005b89e4b6996550ebc6963d65210f8c
Kavindu <156315964+KaviDJ02@users.noreply.github.com>
2025-01-18 23:27:34 +0530

    add functions

---
+++ word-masters.js
@@ -0,0 +1,58 @@
+const letters = document.querySelectorAll('.scoreboard-letter');
+const loadingDiv = document.querySelector('.info-bar ');
+const ANSWER_LENGTH = 5;
+
+
+
+async function init() {
+    let currentGuess = '';
+    let currentRow = 0;
+
+    function commit() {
+        if(currentGuess.length !== ANSWER_LENGTH){
+            //do nothing
+            return;
+        }
+
+        currentRow++;
+        currentGuess = '';
+
+    }
+
+    function backspace() {
+
+    }
+
+    function addLetter(letter) {
+        if (currentGuess.length < ANSWER_LENGTH){
+            currentGuess += letter;
+        } else (
+            currentGuess = currentGuess.substring(0,currentGuess.length - 1) + letter
+        )
+
+        letters[ANSWER_LENGTH * currentRow + currentGuess.length -1].innerText = letter;
+
+    }
+
+    function isLetter(letter) {
+        return /^[a-zA-Z]$/.test(letter);
+    }
+
+    document.addEventListener('keydown', function handleKeyPress (event) {
+        const action = event.key;
+
+        console.log(action);
+        
+        if (action === 'Enter'){
+            commit();
+        } else if (action === 'Backspace'){
+            backspace();
+        } else if (isLetter(action)) {
+            addLetter(action.toUpperCase());
+        } else {
+            //do nothing
+        }
+    });
+}
+
+init();
